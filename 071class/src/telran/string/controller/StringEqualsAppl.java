496042a93dbce7c6bcb2016041d01b502b8e1c2e
Artem Klibaner <32778606+z97@users.noreply.github.com>
2021-01-24 09:40:25 +0200

    Create StringEqualsAppl.java

---
+++ 071class/src/telran/string/controller/StringEqualsAppl.java
@@ -0,0 +1,10 @@
+package telran.string.controller;
+
+public class StringEqualsAppl {
+	public static void main(String[] args) {
+		String str1 = "Hello";
+		String str2 = "Hello";
+		boolean check = str1.equals(str2);
+		System.out.println(check);
+	}
+}
